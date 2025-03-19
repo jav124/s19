4dc3deca7261b865f5f7a5ddbc8ad7acc68d50af
jav124 <158233238+jav124@users.noreply.github.com>
2025-03-19 12:49:15 -0600

    sign-up.page.ts

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,81 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';  
 import { AuthService } from '../auth.service';
-import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]  
 })
 export class SignUpPage implements OnInit {
-  
+  email: string = '';
+  password: string = '';
 
-   username: string= ''; 
-   password: string= ''; 
+  constructor(private router: Router, private authS: AuthService ) { }
 
-  ngOnInit() {
-  }
 
-  constructor(private router:Router,private authService: AuthService,
-    private alertController: AlertController) {
-    
 
-   }
+  ngOnInit() {}
 
-   async onSubmit() {
-    try{
-      await this.authService.register(this.username, this.password);
-      const alert= await this.alertController.create({
-        header: 'Registro exitoso',
-        message: 'Usuario registrado correctamente',
-        buttons: ['OK']
-      });
-      await alert.present();
-      this.router.navigateByUrl('home');
-
+  async onSubmit(){
+    try {
+      await this.authS.register(this.email,this.password);
+      console.log('funciona');
+      this.router.navigate(['/home']);
     } catch (error) {
-      const alert= await this.alertController.create({
-        header: 'Error',
-        message: 'error.message',
-        buttons: ['OK']
-      });
-      await alert.present();
+      console.log('NO funciona');
     }
    }
 
-   validateEmail(email: string): boolean {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-   }
-
-    onSignUp() {
-    this.router.navigateByUrl("sign-up"); 
-
-    }
-  // Agregar un elemento con nombre, fecha y estado inicial (no completado)
-  // addItem(username: string,  password:string,): void {
-  //  if (username.trim() && password.trim() ) {
-  //    this.items.push({
-       
-  //      username: username.trim(),
-  //      password: password.trim(),
-       
-       
-       
-  //    });
-  //    console.log(username.trim(), password.trim());
-  //  } else {
-  //    console.error('El nombre y la fecha no pueden estar vacíos');
-  //  }}
-
-
-  
-  onHome() {
-    this.router.navigateByUrl("home");
+  onSignUp() {
+    console.log('Llendo a Inicio');
+    this.router.navigate(['/home']);
   }
 }
